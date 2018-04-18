f1afb2b41ae0fd22be6253970ea60c3e2fab33c2
Juan Carlos Rodriguez <j.rodriguez.arrieta@accenture.com>
2018-04-17 18:35:05 -0600

    Agregar Test de la clase Pelicula

---
+++ test/com/renta/peliculas/PeliculaTest.java
@@ -0,0 +1,35 @@
+package com.renta.peliculas;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PeliculaTest {
+
+    Pelicula pelicula;
+
+    @Before
+    public void setUp() throws Exception {
+        pelicula = new Pelicula("Tres Chiflados", 100);
+    }
+
+    @Test
+    public void testGetCodigoPrecio() throws Exception {
+        Assert.assertEquals(100, pelicula.getCodigoPrecio());
+    }
+
+    @Test
+    public void testPersistAndGet() throws Exception {
+        pelicula.persist();
+        Assert.assertSame(pelicula, Pelicula.get("Tres Chiflados"));
+    }
+
+    @Test
+    public void testGetAndSetNombre() throws Exception {
+        Assert.assertEquals("Tres Chiflados", pelicula.getNombre());
+        pelicula.setNombre("Lo que el viento se llevó");
+        Assert.assertEquals("Lo que el viento se llevó", pelicula.getNombre());
+    }
+}
